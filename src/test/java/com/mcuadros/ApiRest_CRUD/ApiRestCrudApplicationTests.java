67ba4f28a42f07ca5c9e7d417db97e5843bd546c
MiguelCuadros <miguel.cuadros@vallegrande.edu.pe>
2024-09-04 20:46:42 -0500

    feat: Initial project with Spring Boot

---
+++ src/test/java/com/mcuadros/ApiRest_CRUD/ApiRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcuadros.ApiRest_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
